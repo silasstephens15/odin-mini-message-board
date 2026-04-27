753f578fcde21509f3bd5a13a6dbe07d20e3834d
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2026-04-27 18:56:32 -0400

    feat: added error handling.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { indexRouter } = require("./routes/index.js");
 const newRouter = require("./routes/new.js");
 const path = require("node:path");
+const NotFoundError = require("./errors/NotFoundError.js");
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
@@ -10,6 +11,14 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/new", newRouter);
+app.use((req, res) => {
+  throw new NotFoundError();
+  Next();
+});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
 
 const PORT = 3000;
 app.listen(PORT);
