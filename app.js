8e7f7dacd16b683260c754db127a58d0ae299872
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2026-04-27 19:03:45 -0400

    feat: added support for styling.
    Added basic styling and static resource support.

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const path = require("node:path");
 const NotFoundError = require("./errors/NotFoundError.js");
 
 const app = express();
+const assetsPath = path.join(__dirname, "public");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/new", newRouter);
